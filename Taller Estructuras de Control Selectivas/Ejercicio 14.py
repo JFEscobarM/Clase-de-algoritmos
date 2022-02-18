779403fe08fa2581f1edb182559e2e18af3cb34d
Juan Felipe Escobar <98360727+JFEscobarM@users.noreply.github.com>
2022-02-17 21:34:20 -0500

    Create Ejercicio 14.py

---
+++ Taller Estructuras de Control Selectivas/Ejercicio 14.py
@@ -0,0 +1,24 @@
+"""
+Datos de entrada: 
+Anterior --> float --> A
+Actual --> float --> B 
+
+Datos de salida:
+Total --> float --> C
+"""
+
+# Entrada 
+A = float(input("\nDime la lectura anterior "))
+B = float(input("Dime la lectura actual "))
+D = (B-A)
+
+# Caja negra 
+Valor = [4600, 80000, 100000, 120000]
+lista = [0, 100, 300, 500, 100, 300, 500, 1000000000000000000000000000000000] # El ultimo valor para representa infinito
+
+for i in range(4):
+    if lista[i] <= D <= lista[4 + i]:
+        C = D * Valor[i]
+
+# Salida
+print("La cantidad a pagar es de",C,"\n")
