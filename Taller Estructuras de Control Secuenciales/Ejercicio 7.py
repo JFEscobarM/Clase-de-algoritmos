39f8886f99a6392377ea099d935c2aa4a7081202
Juan Felipe Escobar <98360727+JFEscobarM@users.noreply.github.com>
2022-02-17 21:14:22 -0500

    Create Ejercicio 7.py

---
+++ Taller Estructuras de Control Secuenciales/Ejercicio 7.py
@@ -0,0 +1,13 @@
+"""
+Datos de entrada
+Metros-->int-->Metros
+
+Caja negra
+Pulgadas-->int-->Pulgadas
+Pies-->int-->Pies
+
+"""
+M=int(input("Insertar valor en metros: "))
+Pl=(M*39.27)
+P=(Pl*12)
+print("Metros a pulgadas: ",(Pl), "Metros a pies: ",(P))
